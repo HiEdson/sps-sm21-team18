19fbf2f3910517efb2cc1a4d52ad43ee012f113a
kiana51 <kianaalizediaz@ufl.edu>
2021-07-02 17:05:37 +0000

    Create initial homepage design with google map api and some headers

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -12,3 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
