fbdff1db71d5ad2bf74ab177ebc9913ccdf37268
edson <edsoncasimiro264@gmail.com>
2021-07-08 14:29:40 +0000

    Adding a single-yopic page

--- target/classes/META-INF/resources/script.js
+++ target/classes/META-INF/resources/script.js
@@ -15,38 +15,32 @@
 //fetching data and display then in the list-topics
 async function topics() {
     const getTopics = await fetch('/list-topics');
-    //const getCovid = await fetch('/list-topics?name=covid');
-    //const allCovid = await getCovid.json();
     const allTopics = await getTopics.json();
-    //const myObj = await JSON.parse(allThoughs);
 
-    //const domElement = document.getElementById("allTopics");
     allTopics.forEach(function (o) {
         document.getElementById("allTopics").innerHTML += '<li>' + '<b>' + o.name + '</b>' + '   ' + o.description + '<br>' + '</li>';
     });
-    //domElement.innerHTML = allTopics[Object.keys(allTopics)[0]];
     console.log(allTopics);
-    //allThoughs[Object.keys(allThoughs)[t]];
 
-    //console.log(allThoughs[Object.keys(allThoughs)[t]]);
-    //console.log(myObj.though1); allThoughs.though1;
-    /*let url = new URL('/list-topics?name=covid');
-    let params = new URLSearchParams(url.search);
-    console.log(params.getAll('covid'))*/
 
-    var url = new URL('http://localhost:8080/list-topics');
-    var params = { name: 'covid' }
-    console.log(url.search = new URLSearchParams(params).toString());
-    fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch (function (error) {
-            console.log('request failed', error)
-        });
-
-    //console.log(allCovid + "all covid");
 }
 
 
 
+async function oneTopic() {
+    
+    const OneTopic = await fetch('/one-topic?name=covid');
+    const specificTopic = await OneTopic.json();
+    
+    specificTopic.forEach(function (o) {
+        document.getElementById("oneTopicTitle").innerHTML +=  o.name;
+        document.getElementById("oneTopicDescrip").innerHTML += o.description;
 
+    });
+
+    console.log(specificTopic);
+    //window.location.href ="/topic.html";
+} 
+
+//display the element in the page topic etc....
+//try https://docs.oracle.com/javaee/5/api/javax/servlet/RequestDispatcher.html
